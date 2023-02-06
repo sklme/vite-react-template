b734c9d7ffb063e3b6d55dd69c2ea2f9364dfafa
wrynnsun <wrynnsun@tencent.com>
2023-02-06 20:20:40 +0800

    chore: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import WindiCSS from "vite-plugin-windicss";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import WindiCSS from 'vite-plugin-windicss';
+import path from 'path';
+
+console.log(import.meta);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [WindiCSS(), react()],
+  server: {
+    port: 8007,
+  },
+  resolve: {
+    alias: {
+      '~': `${path.resolve(__dirname, 'src')}`,
+    },
+  },
 });
