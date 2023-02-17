c13153d866a050bcd5a1c5db20c4d428b9efcbb1
wrynnsun <wrynnsun@tencent.com>
2023-02-17 17:17:01 +0800

    feat: reset

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'virtual:windi.css';
 import { RouterProvider } from 'react-router-dom';
 import router from '~/router/router';
-import './index.less';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
